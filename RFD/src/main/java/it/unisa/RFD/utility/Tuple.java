3800d010720a0b605ccb1f9f51f02ed5d013d88e
Giovanni Leo <giovannileo595@gmail.com>
2017-07-27 19:15:55 +0200

    Javadoc Classe tuple

--- RFD/src/main/java/it/unisa/RFD/utility/Tuple.java
+++ RFD/src/main/java/it/unisa/RFD/utility/Tuple.java
@@ -4,7 +4,11 @@ public class Tuple <X,Y>{
 
 	private final X x;
 	private final Y y;
-
+	/**
+	 * @param x
+	 * @param y
+	 * Coppia di elementi immutabili.
+	 */
 	public Tuple(X x, Y y) {
 		this.x = x;
 		this.y = y;
@@ -14,15 +18,23 @@ public class Tuple <X,Y>{
 	public String toString() {
 		return "<" + x + ", " + y +">";
 	}
-
+	
+	/**
+	 * Restituisce il primo elemento della tupla.
+	 * @return primo elemento della tupla
+	 */
 	public X getX() {
 		return x;
 	}
-
+	
+	/**
+	 * Restituisce il secondo elemento della tupla.
+	 * @return primo elemento della tupla
+	 */
 	public Y getY() {
 		return y;
 	}
 
-	
+
 
 }
