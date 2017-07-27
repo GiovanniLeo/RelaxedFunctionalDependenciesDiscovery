4782beec6c9561ac73f3b23d830bdc946d247a33
Luigi Durso <alduin.93y@gmail.com>
2017-07-27 12:21:27 +0200

    Aggiunta DistanceMatrix class
    -Creata la classe DistanceMatrix
    -Creati prototipi metodi loadDF e CheckTypes

---
+++ RFD/src/main/java/it/unisa/RFD/DistanceMatrix.java
@@ -0,0 +1,37 @@
+package it.unisa.RFD;
+
+import java.util.List;
+
+import joinery.DataFrame;
+/**
+ * Classe che provvede alla creazione della matrice delle distanze partendo da un dataframe caricato da file csv
+ * @author luigidurso
+ *
+ */
+public class DistanceMatrix 
+{
+	private List<Class<?>> typesColumn;
+	/**
+	 * Metodo che riceve in input nome del file csv e lo carica in un DataFrame
+	 * @param nameCSV nome file CSV
+	 * @param separetor separatore di colonne utilizzato nel file
+	 * @param hasHeader presenza di header nel file
+	 * @return Dataframe  DataFrame caricato da file
+	 */
+	public static DataFrame<Object> loadDF(String nameCSV,String separetor,boolean hasHeader)
+	{
+		return new DataFrame();
+	}
+	
+	/**
+	 * Metodo che cerca tipo di elemento colonna e restituisce istanza dell'interfaccia per la sottrazione
+	 * @param indiceColonna
+	 * @return Subtraction istanza dell'interfaccia per la sottrazione
+	 */
+	private void checkTypes(int indiceColonna)
+	{
+		
+	}
+	
+
+}
